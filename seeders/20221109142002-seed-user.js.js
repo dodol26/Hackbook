21b3ec0edd83904e84d52ad4cc86424a67702ea4
dodol26 <tonni.lius26@gmail.com>
2022-11-10 05:42:13 +0700

    hash password

--- seeders/20221109142002-seed-user.js.js
+++ seeders/20221109142002-seed-user.js.js
@@ -1,10 +1,15 @@
 'use strict';
 const fs=require('fs')
-let user = JSON.parse(fs.readFileSync('../data/user.json', 'utf8'))
+var bcrypt = require('bcryptjs')
+
+let user = JSON.parse(fs.readFileSync('./data/user.json', 'utf8'))
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
    up (queryInterface, Sequelize) {
     user.forEach(element => {
+      var salt = bcrypt.genSaltSync(10)
+      var hash = bcrypt.hashSync(element.password, salt)
+      element.password = hash
       element.createdAt = new Date()
       element.updatedAt = new Date()
     });
