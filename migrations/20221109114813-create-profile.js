949eb6bef7769ea9a3f57bef4713850f02c3ea36
FianFebryIspianto <hendryfebry01@gmail.com>
2022-11-09 19:02:37 +0700

    update-migration

--- migrations/20221109114813-create-profile.js
+++ migrations/20221109114813-create-profile.js
@@ -26,7 +26,9 @@ module.exports = {
         references: {
           model: 'Users',
           key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       createdAt: {
         allowNull: false,
--- migrations/20221109114904-create-post.js
+++ migrations/20221109114904-create-post.js
@@ -23,7 +23,9 @@ module.exports = {
         references: {
           model: 'Users',
           key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       createdAt: {
         allowNull: false,
