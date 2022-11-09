594512215ee6d849ab91c7c6088b26c0c1f0ba7b
FianFebryIspianto <hendryfebry01@gmail.com>
2022-11-10 00:28:50 +0700

    ADD PUBLIC ASSET,AND FORM LOGIN & REGISTER

--- routers/index.js
+++ routers/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router()
 const Controller = require('../controllers/controller')
 
 router.get('/', Controller.home)
-router.post('/', Controller.addPost)
+// router.post('/', Controller.addPost)
 
-router.get('/register', Controller.registerForm)
-router.post('/register', Controller.register)
+// router.get('/register', Controller.registerForm)
+// router.post('/register', Controller.register)
 
-router.get('/login', Controller.loginForm)
-router.post('/login', Controller.login)
+// router.get('/login', Controller.loginForm)
+// router.post('/login', Controller.login)
 
 module.exports = router
