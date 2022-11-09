37c15684aebddce570d5728839903edbf9656135
dodol26 <tonni.lius26@gmail.com>
2022-11-09 18:38:58 +0700

    Hackbook initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const router = require('./routers')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
+app.use('/', router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
